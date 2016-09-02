28c167742a06c6f74fbbc5682364abb960885696
Adebis <zevsmiles@gmail.com>
2016-09-02 17:02:39 -0400

    Category lists in graph node light

--- Data Entry/Data Entry/Lost Manuscript II Data Entry/GraphNodeLight.cs
+++ Data Entry/Data Entry/Lost Manuscript II Data Entry/GraphNodeLight.cs
@@ -8,20 +8,20 @@ namespace Dialogue_Data_Entry
     class GraphNodeLight
     {
         public int id;
-        public string entity_type;
+        public List<string> entity_type;
 
         public GraphNodeLight()
         {
             id = -1;
-            entity_type = "";
+            entity_type = new List<string>();
         }//end constructor GraphNodeLight
         public GraphNodeLight(Feature base_node)
         {
             id = base_node.Id;
             if (base_node.entity_type.Count > 0)
-                entity_type = base_node.entity_type[0];
+                entity_type = base_node.entity_type;
             else
-                entity_type = "";
+                entity_type = new List<string>();
         }//end constructor GraphNodeLight
     }
 }
